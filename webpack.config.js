ef3fe8a0be56cc11ff71c5581fdcaf9dfa342fc9
Dioniss97 <dioniss1997@gmail.com>
2022-07-28 15:47:38 +0200

    Pendiente ajustar webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,4 +7,24 @@ module.exports = {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist'),
     },
+	mode: 'development',
+    module: {
+    	rules: [
+        {
+            test: /\.(c|sc|sa)ss$/,
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader
+                },
+                'css-loader',
+                'sass-loader'
+            ]
+        }
+      ]
+    },
+	plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'stylesheet.css'
+        })
+    ]
 };
